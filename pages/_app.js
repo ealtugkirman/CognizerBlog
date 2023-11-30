cff9e9823cbb45f3df4427e7715f7d71e1f57cbd
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-11-30 16:31:08 +0300

    hey

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import '../styles/globals.scss';
 import { NextSeo } from 'next-seo';
+import Script from 'next/script';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
@@ -15,7 +16,7 @@ function MyApp({ Component, pageProps }) {
          On this platform, articles and essays on different topics from different fields of cognitive science are published in a way that would bridge the gap between public audience and experts."
         openGraph={{
           title: 'thecognizer.com',
-          description: 'thecognizer.com, dünya genelinde teknolojiyi etkileyen hukuki gelişmeler ile hukuku etkileyen teknolojik gelişmeleri inceler ve aktarır.',
+          description: 'The Cognizer is a publishing platform initiated by CogIST, a cognitive science community from Turkey.',
           images: [{ url: '/your-image-url.jpg' }],
           url: 'https://www.thecognizer.com',
           site_name: 'thecognizer.com',
@@ -26,7 +27,7 @@ function MyApp({ Component, pageProps }) {
           creator: '@the_cognizer',
         }}
         additionalMetaTags={[
-          { name: 'keywords', content: 'hukuk teknolojisi, hukuk start-up, start-up hukuku, hukuk alanında girşimler' },
+          { name: 'keywords', content: 'cognitive, cognitive scince, cogist, the cognizer' },
           { property: 'article:author', content: 'https://www.twitter.com/the_cognizer' },
           { property: 'og:type', content: 'website' },
           { property: 'og:locale', content: 'en' },
@@ -40,6 +41,16 @@ function MyApp({ Component, pageProps }) {
         <link rel="stylesheet" href="/styles/globals.scss" />
       </Head>
       <Layout>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZCTSNPNK6W" />
+        <Script id="google-analytics">
+          { `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-ZCTSNPNK6W');
+        `}
+        </Script>
         <div className="absolute opacity-20 -top-20" />
         <Component {...pageProps} />
       </Layout>
